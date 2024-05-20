89660533d5a3b0597a254eee466e73751c6105d2
juanorigua2 <162354002+juanorigua2@users.noreply.github.com>
2024-05-20 16:23:51 -0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+      
+          
+          "i.imgur.com", "png.pngtree.com", "picsum.photos","cdn-icons-png.freepik.com"
+      
+        ],
+      },
+};
+
+export default nextConfig;
